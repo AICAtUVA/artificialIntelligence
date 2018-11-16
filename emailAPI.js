66b1aacfc35ea783061fe18c5f221c40e6441bac
dswilson4 <dswfrontend@gmail.com>
2018-11-15 20:59:32 -0500

    Added email json

--- emailAPI.js
+++ emailAPI.js
@@ -6,32 +6,53 @@ var http = require('http').Server(mess);
 
 var url = require('url');
 var bodyParser = require('body-parser');
-var path = require("path");
+var path = require('path');
 var nodemailer = require('nodemailer');
 
-
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'brownchickenbrowncow',
-    database: 'safewalktest'
+  host: 'localhost',
+  user: 'root',
+  password: 'brownchickenbrowncow',
+  database: 'safewalktest'
 });
 
-app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.json()); // to support JSON-encoded bodies
 
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+app.use(
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
     extended: true
-}));
+  })
+);
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
 });
 
-app.post('/updateUser', function (req, res) {
-    var name = req.body.name;
-    var phone = req.body.phone;
-    var emailId = req.body.email;
-    var mess = req.body.message;
+app.post('/email', function(req, res) {
+  var name = req.body.name;
+  var phone = req.body.phone;
+  var emailId = req.body.email;
+  var mess = req.body.message;
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'youremail@gmail.com',
+      pass: 'yourpassword'
+    }
+  });
 
+  var mailOptions = {
+    to: 'myfriend@yahoo.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+  };
 
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
 });
